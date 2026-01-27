a0b407d2f6d3fbb98df973784619fd587107756d
Chi Nguyen <buichinguyen1105@gmail.com>
2026-01-27 21:08:03 +0200

    f

---
+++ list.hs
@@ -0,0 +1,43 @@
+myLast :: [a] -> Maybe a
+myLast xs = case xs of
+    [y] -> Just y
+    [] -> Nothing
+    (_:ys) -> myLast ys
+
+myButLast :: [a] -> Maybe a
+myButLast xs = case xs of
+    [y, _] -> Just y
+    [] -> Nothing
+    (_:ys) -> myButLast ys
+
+elementAt :: [a] -> Int -> Maybe a
+elementAt xs n 
+    | n < 1 = Nothing
+    | otherwise = case xs of 
+        [] -> Nothing
+        (y:ys) | n == 1 -> Just y
+        (_:ys) -> elementAt ys (n - 1)
+
+myLength :: [a] -> Int
+myLength xs = helper xs 0 where
+    helper ys rsf = case ys of
+        [] -> rsf
+        (_:rest) -> helper rest (rsf + 1)
+
+myReverse :: [a] -> [a]
+myReverse lst = helper lst [] where
+    helper xs acc = case xs of 
+        [] -> acc
+        (first:rest) -> helper rest (first:acc)
+
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome lst = myReverse lst == lst
+
+data NestedList a = Elem a | List [NestedList a]
+
+flatten :: NestedList a -> [a]
+flatten xs = helper xs [] where
+    helper :: NestedList a -> [a] -> [a]
+    helper node acc = case node of
+        Elem x -> x : acc
+        List items -> foldr helper acc items
