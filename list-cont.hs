29bac17279c70747c70f3ff9936e76bbae48b162
Chi Nguyen <buichinguyen1105@gmail.com>
2026-01-29 14:39:31 +0200

    fodlr, foldl and question 12

--- list-cont.hs
+++ list-cont.hs
@@ -14,3 +14,21 @@ encodeModified xs = helper (pack xs)
     pack [] = []
     pack (x:xs) = (x:same) : pack rest
         where (same,rest) = span (== x) xs
+
+foldr' f acc xs = case xs of 
+    [] -> acc
+    (y:ys) -> f y (foldr' f acc ys)
+
+foldl' f acc xs = case xs of
+    [] -> acc
+    (y:ys) -> foldl' f (f y acc) ys
+
+-- | Decode 
+-- >>> decodeModified [Multiple 4 'a',Single 'b',Multiple 2 'c',Multiple 2 'a',Single 'd',Multiple 4 'e']
+-- "aaaabccaadeeee"
+decodeModified :: [Item a] -> [a]
+decodeModified = foldr' go []
+    where 
+        go (Single val) acc = val : acc
+        go (Multiple count val) acc = replicate count val ++ acc
+
