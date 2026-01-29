4e4f6fae05f0467df6e1204582aced6b5c664d12
Chi Nguyen <buichinguyen1105@gmail.com>
2026-01-29 17:27:45 +0200

    question 13

--- list-cont.hs
+++ list-cont.hs
@@ -1,3 +1,4 @@
+import Control.Monad (when)
 data Item a = Single a | Multiple Int a deriving (Show, Eq)
 -- | 
 -- >>> encodeModified "aaaabccaadeeee"
@@ -32,3 +33,33 @@ decodeModified = foldr' go []
         go (Single val) acc = val : acc
         go (Multiple count val) acc = replicate count val ++ acc
 
+
+
+
+-- |
+-- >>> encodeDirect "aaaabccaadeeee"
+-- [Multiple 4 'a',Single 'b',Multiple 2 'c',Multiple 2 'a',Single 'd',Multiple 4 'e']
+
+encodeDirect :: Eq a => [a] -> [Item a]
+
+-- encodeDirect [] = []
+-- encodeDirect (x:xs) = 
+--     if  count == 1 then Single x : encodeDirect rest
+--                    else Multiple count x : encodeDirect rest
+--     where 
+--         goCount :: Eq a => a -> [a] -> Int -> (Int, [a])
+--         goCount y ys acc = case ys of
+--             [] -> (acc + 1, [])
+--             (first:rest) | first == y -> goCount y rest (acc + 1)
+--                          | otherwise -> (acc + 1, ys)
+--         (count, rest) = goCount x xs 0
+
+encodeDirect = foldr go []
+    where
+        go x [] = [Single x]
+        go x (Single y : ys)
+            | x == y = Multiple 2 x : ys
+            | otherwise = Single x : Single y : ys
+        go x (Multiple n y : ys)
+            | x == y = Multiple (n + 1) y : ys
+            | otherwise = Single x : Multiple n y : ys
